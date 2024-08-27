f36869ed79e305e39b10c8e9ba6f1ac18529084d
Je4nComJ <jeanvictor.fa@gmail.com>
2024-08-27 16:45:15 -0300

    DTO

--- backend/src/main/java/com/gomesdevelopers/desafio_crud/repositories/ClientRepository.java
+++ backend/src/main/java/com/gomesdevelopers/desafio_crud/repositories/ClientRepository.java
@@ -1,9 +1,11 @@
 package com.gomesdevelopers.desafio_crud.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.gomesdevelopers.desafio_crud.entities.Client;
 
+@Repository
 public interface ClientRepository extends JpaRepository<Client, Long>{
 
 }
