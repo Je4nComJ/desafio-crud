af26a63fe6072c37284de0acdd04201927e4c5b0
Je4nComJ <jeanvictor.fa@gmail.com>
2024-08-27 17:33:32 -0300

    find all PAGED

--- backend/src/main/java/com/gomesdevelopers/desafio_crud/services/ClientService.java
+++ backend/src/main/java/com/gomesdevelopers/desafio_crud/services/ClientService.java
@@ -3,6 +3,8 @@ package com.gomesdevelopers.desafio_crud.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,6 +17,13 @@ public class ClientService {
 	
 	@Autowired
 	private ClientRepository repository;
+	
+	@Transactional(readOnly = true)
+	public Page<ClientDTO> findAllPaged(PageRequest pageRequest){
+		Page<Client> list = repository.findAll(pageRequest);
+		
+		return list.map(x -> new ClientDTO(x));		
+	}
 	
 	@Transactional(readOnly = true)
 	public ClientDTO findById(Long id) {
